e45c57fe5d063f24e8af7341594cccc1ccff8c44
CarolineGuterres <carolguterres.silva@gmail.com>
2022-07-08 09:33:17 -0300

    Add files via upload

--- lib/database/paciente_dao.dart
+++ lib/database/paciente_dao.dart
@@ -2,10 +2,25 @@ import 'package:flutter_app_01/model/paciente.dart';
 
 class PacienteDAO{
     static final List<Paciente> _pacientes = []; 
-   // List<Paciente> _p = List<int>.empty(growable: true); // []
- 
-    static adicionar(Paciente p){
+
+    static const String _nomeTabela = 'paciente';
+    static const String _col_id = 'id';
+    static const String _col_nome = 'nome';
+    static const String _col_email = 'email';
+    static const String _col_cartao = 'cartao';
+
+    /*static const String sqlTabelaPaciente = 'CREATE TABELA $_nomeTabela'('
+    '$_col_id INTEGER PRIMARY KEY, '
+    '$_col_nome TEXT , '
+    '$_col_email TEXT , '
+    '$_col_cartao TEXT,'
+    ';)*/
+
+  
+   static adicionar(Paciente p){
         _pacientes.add(p);
+       /* final Database db = await getDatabase();
+        await db.insert(_nomeTabela, p.toMap();)*/
     }
 
     static get listarPacientes{
